d54fdecadab2da0caa9ce49424bdc194f58c772d
Bo Chen <bochen0909@gmail.com>
2025-01-09 23:31:57 +0000

    add timestamp

--- cpp/standard_package/myout.h
+++ cpp/standard_package/myout.h
@@ -3,7 +3,8 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-
+#include <chrono>
+#include <iomanip>
 
 class LogStream : public std::ostream
 {
@@ -91,9 +92,15 @@ public:
     void log(const std::string &msg)
     {
         if (LogStream::verbose) {
+            auto now = std::chrono::system_clock::now();
+            auto time = std::chrono::system_clock::to_time_t(now);
+            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                now.time_since_epoch()) % 1000;
             
             std::stringstream ss;
-            ss << "[info] " << msg;
+            ss << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S")
+               << "." << std::setfill('0') << std::setw(3) << ms.count() 
+               << "] [info] " << msg;
             
             std::cout << ss.str() << std::endl;
         }
