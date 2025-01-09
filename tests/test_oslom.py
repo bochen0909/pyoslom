4a13ec4b31baaebc6f47ac816e23332c791be2b4
Bo Chen <bochen0909@gmail.com>
2025-01-09 23:39:18 +0000

    add pytest

--- tests/test_oslom.py
+++ tests/test_oslom.py
@@ -22,18 +22,18 @@ class Test(unittest.TestCase):
     def test_fit_dense(self):
         clustering = OSLOM(random_state=123)
         clus = clustering.fit_transform(
-            (100 * np.random.random(size=(100, 100))).astype(int)
+            (100 * np.random.random(size=(30, 30))).astype(int)
         )
-        clus = clustering.fit_transform(np.random.random(size=(100, 100)))
+        clus = clustering.fit_transform(np.random.random(size=(30, 30)))
         clus = clustering.fit_transform(
-            np.random.random(size=(100, 100)).astype(np.float32)
+            np.random.random(size=(30, 30)).astype(np.float32)
         )
         self.print_clu(clus)
 
     def test_sparse_matrix(self):
 
         for cls in [csr_matrix, lil_matrix, csc_matrix, coo_matrix]:
-            X = np.random.random(size=(100, 100))
+            X = np.random.random(size=(30, 30))
             X[X < 0.5] = 0
             A = cls(X)
             obj = OSLOM()
@@ -43,7 +43,7 @@ class Test(unittest.TestCase):
     def est_sparse_matrix_directed(self):
 
         for cls in [csr_matrix]:
-            X = np.random.random(size=(100, 100))
+            X = np.random.random(size=(30, 30))
             X[X < 0.3] = 0
             A = cls(X)
             obj = OSLOM(directed=True, verbose=True)
