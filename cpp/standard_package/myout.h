c6b38330910cc7421a319c2bd401896077d7f5cc
Bo Chen <bochen0909@gmail.com>
2025-01-09 22:56:51 +0000

    remove time - error on macos

--- cpp/standard_package/myout.h
+++ cpp/standard_package/myout.h
@@ -3,8 +3,6 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include <chrono>
-#include <iomanip>
 
 
 class LogStream : public std::ostream
@@ -93,15 +91,9 @@ public:
     void log(const std::string &msg)
     {
         if (LogStream::verbose) {
-            auto now = std::chrono::system_clock::now();
-            auto time = std::chrono::system_clock::to_time_t(now);
-            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                now.time_since_epoch()) % 1000;
             
             std::stringstream ss;
-            ss << "[" << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S")
-               << "." << std::setfill('0') << std::setw(3) << ms.count() 
-               << "] [info] " << msg;
+            ss << "[info] " << msg;
             
             std::cout << ss.str() << std::endl;
         }
